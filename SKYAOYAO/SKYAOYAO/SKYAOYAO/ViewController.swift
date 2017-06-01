9abea41aef65d8961f390991e423f0674cdc9d9f
XunLi <xunli@XunLitekiMac-mini.local>
2017-06-01 14:56:40 +0800

    项目的创建

---
+++ SKYAOYAO/SKYAOYAO/SKYAOYAO/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SKYAOYAO
+//
+//  Created by XunLi on 2017/6/1.
+//  Copyright © 2017年 AlexanderYeah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
